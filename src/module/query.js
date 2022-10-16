3c888ae7b055c0c0c93e6a629b264e5008d99ae1
Farhan Kurnia Pratama <93860325+farhankp1202@users.noreply.github.com>
2022-10-16 12:47:56 +0700

    Update query.js

--- src/module/query.js
+++ src/module/query.js
@@ -1,27 +1,29 @@
 'use strict';
 
-const response = {
-   history: [],
-   error: [
-      '> error: please use key ctrl + h or type "help" for help.'
-   ],
-   help: [
-      '[] basic commands',
-      'use [key[ctrl+h]/help] > help',
-      'use [key[ctrl+shift+c]/clear] > clear response',
-      '[] date and time',
-      'today: ctrl -today > (day, dd:mm:yyyy hh:mm:ss:ms timezone_offset)',
-      'date: ctrl [-d/--date] > (day, dd:mm:yyyy)',
-      'time: ctrl [-t/--time] > (hh:mm:ss:ms, timezone_offset)',
-      'day: ctrl [-d/--date] -day > (day)',
-      'date: ctrl [-d/--date] -date > (dd)',
-      'month: ctrl [-d/--date] -month > (mm)',
-      'year: ctrl [-d/--date] -year > (yyyy)',
-      'hour: ctrl [-t/--time] -hour > (hh)',
-      'minute: ctrl [-t/--time] -minute > (mm)',
-      'second: ctrl [-t/--time] -second > (ss)',
-      'milisecond: ctrl [-t/--time] -milisecond > (ms)',
-      'timezone: ctrl [-t/--time] -timezone > (timezone_offset)'
-   ]
+const rs = {
+  history: [],
+  error: {
+    inputError: 'please use key [ctrl + h] or type "sys help" for help.',
+    ifOffline: 'your device has lost internet connection, please check your internet connection and reload this page.'
+  },
+  help: [
+    '[] basic commands',
+    'use [key[ctrl+h]/"sys help"] > help',
+    'use [key[ctrl+shift+c]/"sys clear"] > clear response',
+    '[] date and time',
+    'today: sys -today > (day, dd:mm:yyyy hh:mm:ss:ms timezone_offset)',
+    'date: sys [-d/--date] > (day, dd:mm:yyyy)',
+    'time: sys [-t/--time] > (hh:mm:ss:ms, timezone_offset)',
+    'day: sys [-d/--date] -day > (day)',
+    'date: sys [-d/--date] -date > (dd)',
+    'month: sys [-d/--date] -month > (mm)',
+    'year: sys [-d/--date] -year > (yyyy)',
+    'hour: sys [-t/--time] -hour > (hh)',
+    'minute: sys [-t/--time] -minute > (mm)',
+    'second: sys [-t/--time] -second > (ss)',
+    'milisecond: sys [-t/--time] -milisecond > (ms)',
+    'timezone: sys [-t/--time] -timezone > (timezone_offset)'
+  ]
 }
-export default response;
+
+export {rs};
