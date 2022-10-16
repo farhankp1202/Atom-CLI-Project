f4b626e7557b4fd2562f2c6104108e6a67e056e5
Farhan Kurnia Pratama <93860325+farhankp1202@users.noreply.github.com>
2022-10-16 12:44:56 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,66 +1,59 @@
 'use strict';
 
-import * as func from './module/configs.js';
+import {rs} from './module/query.js';
+import * as fn from './module/function.js';
 
-window.addEventListener('load', (inp, res) => {
-   'use strict';
-   inp = document.querySelector('#input');
-   res = document.querySelector('#response');
-   let el = ['label', 'input'];
-   el.forEach((val) => {
-      inp.appendChild(
-         document.createElement(val)
-      )
-   });
-   inp.children[0].appendChild(
-      document.createTextNode('@')
-   );
-   let atr = ['type', 'spellcheck', 'autocomplete', 'autocapitalize'];
-   let val = ['text', 'false', 'off', 'off'];
-   
-   for(let i = 0; i < atr.length; i++) {
-      inp.children[1].setAttribute(
-         atr[i],
-         val[i]
-      )
-   }
-   inp.children[1].addEventListener(
-      'keydown', (key) => {
-         'use strict';
-         key = event.key;
-         if(key == 'Enter') {
-            response();
-            console.log('@ '+inp.children[1].value)
-            return inp.children[1].value = '';
-         } else {return}
-      }
-   );
-   function response() {
+window.addEventListener('load', () => {
+  'use strict';
+  let inp = document.querySelector('#input'),
+  res = document.querySelector('#response'),
+  el = ['label', 'input'];
+  el.forEach((val) => {
+    inp.appendChild(
+      document.createElement(val)
+    )
+  });
+  inp.children[0].appendChild(
+    document.createTextNode('@')
+  );
+  let atr = ['type', 'spellcheck', 'autocomplete', 'autocapitalize'],
+  val = ['text', 'false', 'off', 'off'];
+
+  for(let i = 0; i < atr.length; i++) {
+    inp.children[1].setAttribute(
+      atr[i],
+      val[i]
+    )
+  }
+  inp.children[1].addEventListener(
+    'keydown', (key) => {
       'use strict';
-      let que = inp.children[1].value,
-      res = document.querySelector('#response'),
-      rst = func.que.query(que);
-      
-      switch (que) {
-         case 'reload':
-            return func.que.reload(document);
-            break;
-         case 'help':
-            return func.que.help(
-               func.que.time('full_time'),
-               res, 
-               func.response.help);
-            break;
-         case 'clear':
-            return func.que.clear(res);
-            break;
-         default:
-            return func.que.error(
-               func.que.time('full_time'),
-               res, func.response.error,
-               inp.children[1].value
-            );
-            break;
+      key = event.key;
+      if(key == 'Enter') {
+        response();
+        console.log('@ '+inp.children[1].value)
+        return inp.children[1].value = '';
+      } else {return}
+    }
+  );
+  function response() {
+    'use strict';
+    let a = inp.children[1].value,
+    b = document.querySelector('#response'),
+    c = fn.query(a);
+
+    switch(c) {
+      case 'reload':
+        return fn.reload(window);
+        break;
+      case 'help':
+        return fn.help(b, rs.help, fn.time('full_time'));
+        break;
+      case 'clear':
+        return fn.clear(b);
+        break;
+      default:
+        return fn.error('error_input', b, a, rs.error.inputError, fn.time('full_time'));
       }
    }
 })
